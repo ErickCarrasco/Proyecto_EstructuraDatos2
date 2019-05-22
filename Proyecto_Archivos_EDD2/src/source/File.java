471dad91c8740d3dd07d7c60b783e315967e434b
unknown <erick.e.carrasco88@gmail.com>
2019-05-22 12:39:21 -0600

    Classes para administrar agregadas

---
+++ Proyecto_Archivos_EDD2/src/source/File.java
@@ -0,0 +1,31 @@
+
+package source;
+import java.io.Serializable;
+import java.util.ArrayList;
+/**
+ *
+ * @author Erick C
+ */
+public class File implements Serializable{
+    private static final long SerialVersionUID =7899L;
+    private String fileName;
+    
+    public void createFile(String cFile){
+        this.fileName = cFile;  
+        
+    }
+    
+    public void saveFile(){
+        
+    }
+    
+    public void openFile(){
+        boolean ifopen;
+        
+        
+    }
+    
+    public void closeFile(){
+        
+    }
+}
