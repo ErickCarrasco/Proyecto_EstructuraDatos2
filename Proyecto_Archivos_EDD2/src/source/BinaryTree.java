0aff3721231eb24fed44af416a6f36c1c4375ad0
unknown <erick.e.carrasco88@gmail.com>
2019-05-22 10:40:12 -0600

    Editado de BinaryTree y BinaryTreeNodes

--- Proyecto_Archivos_EDD2/src/source/BinaryTree.java
+++ Proyecto_Archivos_EDD2/src/source/BinaryTree.java
@@ -5,6 +5,34 @@ package source;
  *
  * @author Erick C
  */
-public class BinaryTree {
-    //private 
+public class BinaryTree <T>{
+    private BinaryTreeNode<T> root;
+    
+    public BinaryTreeNode getRoot(){
+        return root;
+    }
+    
+    public void insert(){
+        BinaryTreeNode temp = new BinaryTreeNode();
+        temp = root;
+        
+        if (root==null) {
+            BinaryTreeNode nRoot = null;
+            //nRoot.setData(root);
+            root = nRoot;
+            
+        }else{
+            BinaryTreeNode leftC = new BinaryTreeNode();
+            leftC = root.getLeft();
+            BinaryTreeNode rightC = new BinaryTreeNode();
+            rightC = root.getRigh();
+            
+        }
+        
+    }
+    
+    public void delete(){
+        
+    }
+    
 }
--- Proyecto_Archivos_EDD2/src/source/BinaryTreeNode.java
+++ Proyecto_Archivos_EDD2/src/source/BinaryTreeNode.java
@@ -4,6 +4,50 @@ package source;
  *
  * @author Erick C
  */
-public class BinaryTreeNode {
+public class BinaryTreeNode <T> {
+    private BinaryTreeNode left;
+    private BinaryTreeNode right;
+    private T data;
+    
+    public BinaryTreeNode(){
+        this.left=null;
+        this.right=null;
+        this.data=data;
+    }
+    
+    public BinaryTreeNode(BinaryTreeNode left, BinaryTreeNode right, T data){
+        this.left=left;
+        this.right=right;
+        this.data=data;
+    }
+    
+    //Getters
+    public BinaryTreeNode getLeft(){
+        return left;
+    }
+    
+    public BinaryTreeNode getRigh(){
+        return right;
+    }
+    
+    public T getData(){
+        return data;
+    }
+    
+    //Setters
+    
+    public void setLeft(BinaryTreeNode sLeft){
+        this.left=sLeft;
+    }
+    
+    public void setRight(BinaryTreeNode sRight){
+        this.right=sRight;
+    }
+    
+    public void setData(T sData){
+        this.data= sData;
+    }
+    
+    
     
 }
